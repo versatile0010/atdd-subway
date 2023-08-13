15b6c530f856c86be27e925ebab381930c1e83c4
versatile0010 <siam0294@naver.com>
2023-08-14 02:12:29 +0900

    feat: 역 생성 DTO 생성자 관련 애노테이션 추가

--- src/main/java/kuit/subway/dto/request/CreateStationRequest.java
+++ src/main/java/kuit/subway/dto/request/CreateStationRequest.java
@@ -1,7 +1,11 @@
 package kuit.subway.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class CreateStationRequest {
     private String name;
--- src/main/java/kuit/subway/dto/response/CreateStationResponse.java
+++ src/main/java/kuit/subway/dto/response/CreateStationResponse.java
@@ -1,7 +1,11 @@
 package kuit.subway.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class CreateStationResponse {
     private Long id;
