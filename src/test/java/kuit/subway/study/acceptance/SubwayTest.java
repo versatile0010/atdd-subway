672c14cabb674426d2f1162d5b8e1fdeeee47bfc
versatile0010 <siam0294@naver.com>
2023-08-14 02:13:31 +0900

    feat: 역 생성 인수테스트 request 타입을 json 객체로 변경

--- src/test/java/kuit/subway/study/acceptance/SubwayTest.java
+++ src/test/java/kuit/subway/study/acceptance/SubwayTest.java
@@ -1,25 +1,23 @@
 package kuit.subway.study.acceptance;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kuit.subway.AcceptanceTest;
+import kuit.subway.dto.request.CreateStationRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class SubwayTest extends AcceptanceTest {
     @Test
     public void 지하철_역_생성() {
         // given
-        Map<String, String> body = new HashMap<>();
-        body.put("name", "강남역");
+        CreateStationRequest request = new CreateStationRequest("강남역");
         // when
         ExtractableResponse<Response> extract = RestAssured.given().log().all()
-                .body(body)
-                .when().post("stations")
+                .body(request).contentType(ContentType.JSON)
+                .when().post("/stations")
                 .then().log().all()
                 .extract();
         // then
