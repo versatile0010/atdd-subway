2d8cb47adbede5c12b0045202ee81dda9d25ba9d
versatile0010 <siam0294@naver.com>
2023-08-14 01:42:36 +0900

    feat: Station 엔티티 구현

---
+++ src/main/java/kuit/subway/domain/Station.java
@@ -0,0 +1,24 @@
+package kuit.subway.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "STATION")
+public class Station {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Builder
+    public Station(String name) {
+        this.name = name;
+    }
+}
