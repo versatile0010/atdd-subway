fb9409ae2eac716c7c96c99868e1cb0148548a06
versatile0010 <siam0294@naver.com>
2023-08-14 02:12:48 +0900

    feat: 역 생성 API 구현

--- src/main/java/kuit/subway/controller/StationController.java
+++ src/main/java/kuit/subway/controller/StationController.java
@@ -1,13 +1,30 @@
 package kuit.subway.controller;
 
+import kuit.subway.dto.request.CreateStationRequest;
+import kuit.subway.dto.response.CreateStationResponse;
 import kuit.subway.service.StationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/stations")
 @RestController
 public class StationController {
     private final StationService stationService;
+
+    @PostMapping
+    public ResponseEntity<CreateStationResponse> createStation(
+            @RequestBody CreateStationRequest request
+    ) {
+        String name = request.getName();
+        log.info("[지하철 " + name + " 역 생성 API 를 호출합니다.]");
+        CreateStationResponse response = stationService.createOne(name);
+        return ResponseEntity.ok(response);
+    }
 }
