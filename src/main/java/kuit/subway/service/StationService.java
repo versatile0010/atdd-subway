2cefdc160879e0d97339f9c3876635ced0b0bdaa
versatile0010 <siam0294@naver.com>
2023-08-14 02:12:02 +0900

    feat: 역 생성 비즈니스 로직 구현

--- src/main/java/kuit/subway/service/StationService.java
+++ src/main/java/kuit/subway/service/StationService.java
@@ -1,5 +1,7 @@
 package kuit.subway.service;
 
+import kuit.subway.domain.Station;
+import kuit.subway.dto.response.CreateStationResponse;
 import kuit.subway.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class StationService {
     private final StationRepository stationRepository;
+
+    @Transactional
+    public CreateStationResponse createOne(String name) {
+        Station savedStation = stationRepository.save(
+                Station.builder()
+                        .name(name)
+                        .build());
+        return new CreateStationResponse(savedStation.getId());
+    }
 }
