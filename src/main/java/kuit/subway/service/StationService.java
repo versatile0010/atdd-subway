ae783d73af1ad2a3d182069f8452984173eb2693
versatile0010 <siam0294@naver.com>
2023-08-14 15:38:16 +0900

    feat: 지하철 목록 조회 비지니스 로직 구현

--- src/main/java/kuit/subway/service/StationService.java
+++ src/main/java/kuit/subway/service/StationService.java
@@ -2,10 +2,14 @@ package kuit.subway.service;
 
 import kuit.subway.domain.Station;
 import kuit.subway.dto.response.CreateStationResponse;
+import kuit.subway.dto.response.StationDto;
+import kuit.subway.dto.response.StationListResponse;
 import kuit.subway.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -20,5 +24,13 @@ public class StationService {
                         .name(name)
                         .build());
         return new CreateStationResponse(savedStation.getId());
+    }
+
+    public StationListResponse getStations() {
+        List<Station> savedStations = stationRepository.findAll();
+        List<StationDto> stations = savedStations.stream().map(
+                StationDto::new
+        ).toList();
+        return new StationListResponse(stations);
     }
 }
