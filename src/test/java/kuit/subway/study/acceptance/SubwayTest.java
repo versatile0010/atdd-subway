560e81afdbcf73b7ca3672e708220c283438bca4
versatile0010 <siam0294@naver.com>
2023-08-14 18:05:00 +0900

    refactor: SubwayTest

--- src/test/java/kuit/subway/study/acceptance/SubwayTest.java
+++ src/test/java/kuit/subway/study/acceptance/SubwayTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import jdk.jfr.Description;
 import kuit.subway.AcceptanceTest;
 import kuit.subway.dto.request.CreateStationRequest;
 import kuit.subway.dto.response.CreateStationResponse;
@@ -18,7 +19,9 @@ import static org.hamcrest.Matchers.equalTo;
 public class SubwayTest extends AcceptanceTest {
     @Autowired
     StationService stationService;
+    private final String PATH = "/stations";
 
+    @Description("지하철 역 생성 API 인수 테스트")
     @Test
     public void 지하철_역_생성() {
         // given
@@ -26,15 +29,16 @@ public class SubwayTest extends AcceptanceTest {
         // when
         ExtractableResponse<Response> extract = RestAssured.given().log().all()
                 .body(request).contentType(ContentType.JSON)
-                .when().post("/stations")
+                .when().post(PATH)
                 .then().log().all()
                 .extract();
         // then
-        Assertions.assertEquals(200, extract.statusCode());
+        Assertions.assertEquals(HttpStatus.OK.value(), extract.statusCode());
     }
 
+    @Description("지하철 목록 조회 API 인수 테스트")
     @Test
-    public void 지하철_목록_조회() throws Exception {
+    public void 지하철_목록_조회() {
         // given Given 2개의 지하철역을 생성하고
         stationService.createOne("강남역");
         stationService.createOne("서초역");
@@ -42,26 +46,27 @@ public class SubwayTest extends AcceptanceTest {
         // when 지하철 목록을 조회하면
         // then 2 개의 지하철 역 목록을 응답받는다.
         RestAssured.given().log().all()
-                .when().get("/stations")
+                .when().get(PATH)
                 .then().log().all()
                 .assertThat().body("stations.size()", equalTo(2))
-                .assertThat().statusCode(200);
+                .assertThat().statusCode(HttpStatus.OK.value());
     }
 
+    @Description("지하철 삭제 API 인수 테스트")
     @Test
-    public void 지하철_삭제() throws Exception {
+    public void 지하철_삭제() {
         // given 지하철 역을 생성하고
         CreateStationResponse station = stationService.createOne("강남역");
 
         // when 그 지하철 역을 삭제하면
         RestAssured.given().log().all()
-                .when().delete("/stations/{id}", String.valueOf(station.getId()))
+                .when().delete(PATH + "/{id}", String.valueOf(station.getId()))
                 .then().log().all()
                 .assertThat().statusCode(HttpStatus.OK.value());
 
         // then 그 지하철 역 목록 조회 시 생성한 역을 찾을 수 없다.
         RestAssured.given().log().all()
-                .when().get("/stations")
+                .when().get(PATH)
                 .then().log().all()
                 .assertThat().statusCode(HttpStatus.NO_CONTENT.value());
     }
