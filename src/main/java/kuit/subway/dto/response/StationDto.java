51dc57699db21ed9b8301da87b6795c9e09557a1
versatile0010 <siam0294@naver.com>
2023-08-14 15:37:59 +0900

    feat: 지하철 목록 조회 DTO 생성

---
+++ src/main/java/kuit/subway/dto/response/StationDto.java
@@ -0,0 +1,19 @@
+package kuit.subway.dto.response;
+
+import kuit.subway.domain.Station;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class StationDto {
+    private Long id;
+    private String name;
+
+    public StationDto(Station station) {
+        this.id = station.getId();
+        this.name = station.getName();
+    }
+}
