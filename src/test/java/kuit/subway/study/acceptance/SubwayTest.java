23aa73bc2956d24127934b110d06d51e803937d1
versatile0010 <siam0294@naver.com>
2023-08-14 15:37:38 +0900

    feat: 지하철 목록 조회 인수테스트 작성

--- src/test/java/kuit/subway/study/acceptance/SubwayTest.java
+++ src/test/java/kuit/subway/study/acceptance/SubwayTest.java
@@ -6,10 +6,17 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kuit.subway.AcceptanceTest;
 import kuit.subway.dto.request.CreateStationRequest;
+import kuit.subway.service.StationService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.Matchers.equalTo;
 
 public class SubwayTest extends AcceptanceTest {
+    @Autowired
+    StationService stationService;
+
     @Test
     public void 지하철_역_생성() {
         // given
@@ -22,5 +29,20 @@ public class SubwayTest extends AcceptanceTest {
                 .extract();
         // then
         Assertions.assertEquals(200, extract.statusCode());
+    }
+
+    @Test
+    public void 지하철_목록_조회() throws Exception {
+        // given Given 2개의 지하철역을 생성하고
+        stationService.createOne("강남역");
+        stationService.createOne("서초역");
+
+        // when 지하철 목록을 조회하면
+        // then 2 개의 지하철 역 목록을 응답받는다.
+        RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .assertThat().body("stations.size()", equalTo(2))
+                .assertThat().statusCode(200);
     }
 }
