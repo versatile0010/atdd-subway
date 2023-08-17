35fbb07f15950e5e23f5d82eb966dbe1fa7d1fda
versatile0010 <siam0294@naver.com>
2023-08-17 16:49:00 +0900

    feat: AcceptanceFixture 구현

---
+++ src/test/java/kuit/subway/study/acceptance/AcceptanceFixture.java
@@ -0,0 +1,33 @@
+package kuit.subway.study.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kuit.subway.dto.request.CreateStationRequest;
+
+public class AcceptanceFixture {
+    private static final String STATION_PATH = "/stations";
+
+    public static ExtractableResponse<Response> 지하철_역_생성하기(CreateStationRequest request) {
+        return RestAssured.given().log().all()
+                .body(request).contentType(ContentType.JSON)
+                .when().post(STATION_PATH)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철_역_목록_조회하기() {
+        return RestAssured.given().log().all()
+                .when().get(STATION_PATH)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철_역_삭제하기(int 지하철_역_아이디) {
+        return RestAssured.given().log().all()
+                .when().delete(STATION_PATH + "/{id}", 지하철_역_아이디)
+                .then().log().all()
+                .extract();
+    }
+}
