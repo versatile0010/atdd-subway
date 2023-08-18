760ccec4f311a1fb1975a3306b129d6417da38d0
versatile0010 <siam0294@naver.com>
2023-08-18 17:26:52 +0900

    feat: 노선(Line) 생성 요청 데이터 픽스쳐 구현

--- src/test/java/kuit/subway/study/acceptance/StationFixData.java
+++ src/test/java/kuit/subway/study/acceptance/StationFixData.java
@@ -1,9 +1,14 @@
 package kuit.subway.study.acceptance;
 
+import kuit.subway.dto.request.CreateLineRequest;
 import kuit.subway.dto.request.CreateStationRequest;
 
 public class StationFixData {
-    public static CreateStationRequest 지하철_역_생성_데이터(String name){
+    public static CreateStationRequest 지하철_역_생성_데이터(String name) {
         return new CreateStationRequest(name);
+    }
+
+    public static CreateLineRequest 지하철_노선_생성_데이터(String name, String color, int distance, Long downStationId, Long upStationId) {
+        return new CreateLineRequest(color, distance, name, downStationId, upStationId);
     }
 }
