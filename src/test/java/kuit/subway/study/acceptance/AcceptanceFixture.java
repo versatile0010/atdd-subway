9cdbcfe5219f032b994b6647fd7ff63a549b3106
versatile0010 <siam0294@naver.com>
2023-08-18 17:26:30 +0900

    feat: 노선(Line) 생성 픽스쳐 구현

--- src/test/java/kuit/subway/study/acceptance/AcceptanceFixture.java
+++ src/test/java/kuit/subway/study/acceptance/AcceptanceFixture.java
@@ -4,10 +4,12 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import kuit.subway.dto.request.CreateLineRequest;
 import kuit.subway.dto.request.CreateStationRequest;
 
 public class AcceptanceFixture {
     private static final String STATION_PATH = "/stations";
+    private static final String LINE_PATH = "/lines";
 
     public static ExtractableResponse<Response> 지하철_역_생성하기(CreateStationRequest request) {
         return RestAssured.given().log().all()
@@ -24,9 +26,16 @@ public class AcceptanceFixture {
                 .extract();
     }
 
-    public static ExtractableResponse<Response> 지하철_역_삭제하기(int 지하철_역_아이디) {
+    public static ExtractableResponse<Response> 지하철_역_삭제하기(Long 지하철_역_아이디) {
         return RestAssured.given().log().all()
                 .when().delete(STATION_PATH + "/{id}", 지하철_역_아이디)
+                .then().log().all()
+                .extract();
+    }
+    public static ExtractableResponse<Response> 지하철_노선_생성하기(CreateLineRequest request) {
+        return RestAssured.given().log().all()
+                .body(request).contentType(ContentType.JSON)
+                .when().post(LINE_PATH)
                 .then().log().all()
                 .extract();
     }
