d484f3df8d320b13a73694a1f22f29212916b79b
versatile0010 <siam0294@naver.com>
2023-08-24 15:45:54 +0900

    feat: 마지막 구간 삭제 시, 하행종점역을 갱신

--- src/main/java/kuit/subway/service/LineService.java
+++ src/main/java/kuit/subway/service/LineService.java
@@ -114,8 +114,11 @@ public class LineService {
         Line line = lineRepository.findById(id)
                 .orElseThrow(NotFoundLineException::new);
         Station downStation = section.getDownStation();
-
+        Station upStation = section.getUpStation();
         line.removeSection(downStation.getId()); // section 제거
+        // 현재 요구사항에서는 하행종점역만 삭제 가능하므로 stationId 를 인자로 받도록 하였음.
+        line.setDownStationId(upStation.getId());
+        // 하행종점역을 삭제했으므로 해당 라인의 하행종점을 갱신
         return new DeleteSectionResponse(request.getSectionId()); // 제거한 section id 를 반환
     }
 }
