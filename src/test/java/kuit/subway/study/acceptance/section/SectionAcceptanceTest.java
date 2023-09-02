ff979b22d5eeb812bba2190d51a68117fc1fe767
versatile0010 <siam0294@naver.com>
2023-09-02 17:39:00 +0900

    feat: @Description 어노테이션을 @DisplayName 으로 변경

--- src/test/java/kuit/subway/study/acceptance/section/SectionAcceptanceTest.java
+++ src/test/java/kuit/subway/study/acceptance/section/SectionAcceptanceTest.java
@@ -2,11 +2,11 @@ package kuit.subway.study.acceptance.section;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import jdk.jfr.Description;
 import kuit.subway.AcceptanceTest;
 import kuit.subway.dto.request.CreateSectionRequest;
 import kuit.subway.dto.request.CreateStationRequest;
 import kuit.subway.dto.request.DeleteSectionRequest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -24,11 +24,8 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     private final Long 강남역_아이디 = 1L;
     private final Long 서초역_아이디 = 2L;
     private final Long 이호선_아이디 = 1L;
-    private final int SECTION_AT_BETWEEN = 2;
-    private final int SECTION_AT_FIRST = 1;
-    private final int SECTION_AT_LAST = 0;
 
-    @Description("지하철 구간 생성 요청이 올바르면, 구간이 생성되어야 한다.")
+    @DisplayName("지하철 구간 생성 요청이 올바르면, 구간이 생성되어야 한다.")
     @Test
     public void 지하철_구간_생성_테스트() {
         // given 강남역하행과 서초역상행인 2호선을 생성하고 (서초역 -> 강남역)
@@ -42,11 +39,13 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 
         ExtractableResponse<Response> extract = 지하철_구간_생성하기(구간_데이터, 이호선_아이디);
         // then 구간이 생성되고 노선 Id 를 응답으로 받는다. {서초역 -> 강남역 -> 건대입구역}
+
+        지하철_노선_조회하기(이호선_아이디);
         응답결과_검증하기(extract, HttpStatus.CREATED);
         응답바디값_기대값과_같은지_검증하기(extract, "id", 1);
     }
 
-    @Description("새로운 구간의 하행역이 기존 구간에 존재하는 역이면, 구간 생성 요청이 거절되어야 합니다.")
+    @DisplayName("새로운 구간의 하행역이 기존 구간에 존재하는 역이면, 구간 생성 요청이 거절되어야 합니다.")
     @Test
     public void 지하철_구간_중복역_생성_테스트() {
         // given 강남역하행과 서초역상행인 2호선을 생성하고 (서초역 -> 강남역)
@@ -58,7 +57,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         응답결과_검증하기(extract, HttpStatus.BAD_REQUEST);
     }
 
-    @Description("새로운 구간의 상행역이 기존 구간의 하행 종점이 아니라면, 구간 생성 요청이 거절되어야 합니다.")
+    @DisplayName("새로운 구간의 상행역이 기존 구간의 하행 종점이 아니라면, 구간 생성 요청이 거절되어야 합니다.")
     @Test
     public void 지하철_구간_생성_예외_테스트() {
         // given 강남역하행과 서초역상행인 2호선을 생성하고 (서초역 -> 강남역)
@@ -78,7 +77,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         응답결과_검증하기(extract, HttpStatus.BAD_REQUEST);
     }
 
-    @Description("마지막 구간에 대한 삭제 요청 시, 구간이 제거되어야 한다.")
+    @DisplayName("마지막 구간에 대한 삭제 요청 시, 구간이 제거되어야 한다.")
     @Test
     public void 지하철_하행종점_구간_삭제_테스트() {
         /* given 강남역하행과 서초역상행인 2호선을 생성하고 (서초역 -> 강남역)
@@ -102,7 +101,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         지하철_노선_조회하기(이호선_아이디);
     }
 
-    @Description("마지막 구간이 아닌 구간에 대한 삭제 요청은, 거절되어야 한다.")
+    @DisplayName("마지막 구간이 아닌 구간에 대한 삭제 요청은, 거절되어야 한다.")
     @Test
     public void 지하철_하행종점이_아닌_구간_삭제_테스트() {
         /* given 강남역하행과 서초역상행인 2호선을 생성하고 (서초역 -> 강남역)
