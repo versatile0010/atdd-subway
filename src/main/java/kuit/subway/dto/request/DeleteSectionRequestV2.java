d9a09781efa2d0ab5eba9417dc99998b4afa3bea
versatile0010 <siam0294@naver.com>
2023-09-05 09:46:41 +0900

    feat: 새로운 구간 삭제 api 에 대한 요청 dto 생성

---
+++ src/main/java/kuit/subway/dto/request/DeleteSectionRequestV2.java
@@ -0,0 +1,16 @@
+package kuit.subway.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class DeleteSectionRequestV2 {
+    @NotNull(message = "새로운 구간의 하행역은 null 일 수 없습니다.")
+    private Long downStationId;
+    @NotNull(message = "새로운 구간의 상행역은 null 일 수 없습니다.")
+    private Long upStationId;
+}
